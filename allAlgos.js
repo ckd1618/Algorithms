fee197c9f0eaf2e0f68a155dc9125247abe74359
ckd1618 <ckd1618@gmail.com>
2019-03-01 01:28:00 -0800

    updated allAlgos.js

--- allAlgos.js
+++ allAlgos.js
@@ -4,3 +4,13 @@ function factRec(num) {
 	return num*factRec(num-1);
 }
 console.log(factRec(5));
+
+//FACTORIAL ITERATIVE
+function factIter(num) {
+	total = 1;
+	for (var i=num; i>1; i--) {
+		total = total * i;
+	}
+	return total;
+}
+console.log(factIter(5));
