8a5f08d20f8bbd1b0d9601bb8714f9bfec8490c0
ckd1618 <ckd1618@gmail.com>
2019-03-01 01:07:14 -0800

    added factorial_iterative.js

---
+++ factorial_iterative.js
@@ -0,0 +1,9 @@
+function factIter(num) {
+	total = 1;
+	for (var i=num; i>1; i--) {
+		total = total * i;
+	}
+	return total;
+}
+
+console.log(factIter(5));
--- factorial_recursive.js
+++ factorial_recursive.js
@@ -1,5 +1,5 @@
 function factRec(num) {
-	if (num === 1) return 1;
+	if (num === 2) return 2;
 	return num*factRec(num-1);
 }
 
