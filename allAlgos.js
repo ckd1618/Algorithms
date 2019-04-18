cc7949917affe0200e4e377539d048e6a8ddf336
ckd1618 <ckd1618@gmail.com>
2019-04-18 10:03:54 -0700

    added factorial to AllAlgos.js

--- allAlgos.js
+++ allAlgos.js
@@ -3,14 +3,14 @@
 //FACTORIAL RECURSIVE
 function factRec(num) {
 	if (num === 2) return 2;
-	return num*factRec(num-1);
+	return num * factRec(num - 1);
 }
 console.log(factRec(5));
 
 //FACTORIAL ITERATIVE
 function factIter(num) {
 	total = 1;
-	for (var i=num; i>1; i--) {
+	for (var i = num; i > 1; i--) {
 		total = total * i;
 	}
 	return total;
@@ -18,25 +18,33 @@ function factIter(num) {
 console.log(factIter(5));
 
 //FIBONACCI RECURSIVE - MINE = 0(n)
-function fibRec(num, first=0, sec=1) {
+function fibRec(num, first = 0, sec = 1) {
 	if (num === 1) return 0;
-	if (num <= 3) return first+sec;
-	return fibRec(num-1, sec, first+sec);
+	if (num <= 3) return first + sec;
+	return fibRec(num - 1, sec, first + sec);
 }
 console.log(fibRec(10));
 
+//FIBONACCI RECURSIVE - MINE but start 1,1,2 = 0(n)
+var fib = function (N, first = 1, sec = 1) {
+	if (N === 0) return 0;
+	if (N <= 2) return 1;
+	if (N == 3) return first + sec;
+	return fib(N - 1, sec, first + sec);
+};
+
 //FIBONACCI RECURSIVE 1ARG = O(2^n)
 function fibRec2(num) {
-	if (num <= 2) return num-1;
-	return fibRec2(num-1) + fibRec2(num-2);
+	if (num <= 2) return num - 1;
+	return fibRec2(num - 1) + fibRec2(num - 2);
 }
 console.log(fibRec2(4));
 
 //FIBONACCI ITERATIVE = O(n)
 function fibIter(num) {
 	if (num < 2) return num;
-	var [first,sec,sum] = [0,1,0];
-	for (var i=1; i<num; i++) {
+	var [first, sec, sum] = [0, 1, 0];
+	for (var i = 1; i < num; i++) {
 		var sum = first + sec;
 		first = sec;
 		sec = sum;
@@ -47,27 +55,27 @@ console.log(fibIter(0));
 
 //REVERSE STRING ITERATIVE
 function reverseString(str) {
-  var newStr = '';
-  for (var i = str.length-1; i >= 0; i--) {
-    newStr = newStr + str[i];
-  }
-  return newStr;
+	var newStr = '';
+	for (var i = str.length - 1; i >= 0; i--) {
+		newStr = newStr + str[i];
+	}
+	return newStr;
 }
 console.log(reverseString('yoyo mastery'));
 
 //REVERSE STRING RECURSIVE
-function reverseString(str, i=str.length-1) {
-  if (i <= 0) return str[i] || '';
-  return str[i] + reverseString(str, i-1);
+function reverseString(str, i = str.length - 1) {
+	if (i <= 0) return str[i] || '';
+	return str[i] + reverseString(str, i - 1);
 }
 console.log(reverseString('yoyo mastery'));
 
 //REVERSE STRING RECURSIVE 1ARG
-function reverseStringRecursive (str) {
-  if (str === "") {
-    return "";
-  } else {
-    return reverseStringRecursive(str.substr(1)) + str[0];
-  }
+function reverseStringRecursive(str) {
+	if (str === "") {
+		return "";
+	} else {
+		return reverseStringRecursive(str.substr(1)) + str[0];
+	}
 }
 reverseStringRecursive('yoyo master');
