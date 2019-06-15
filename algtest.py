3fb47039d49283701bddd4631a8ae43a4df9ffad
ckd1618 <ckd1618@gmail.com>
2019-06-15 13:28:18 -0700

    key added

--- algtest.py
+++ algtest.py
@@ -1,2 +1,54 @@
-class Node(obj):
+
+class Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.nextNode = None
+
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+# O(1)
+    def insertStart(self, data):
+
+        self.size = self.size +1
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+
+    def remove(self, data):
+
+        if self.head is None:
+            return
+        
+        self.size = self.size-1
+
+        currentNode = self.head
+        previousNode = None
+
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+        
+        if previousNode is None:
+            self.head = currentNode.nextNode
+        else:
+            previousNode.nextNode = currentNode.nextNode
+    
+    #O(1)
+    def size1(self):
+        return self.size
+    
+    #O(N)
+    def size2(self):
+
+        actualNode = self.head
+        size = 0
     
