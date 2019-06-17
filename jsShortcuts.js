0b2006659a44db44557ff028bbfef8486da8204d
ckd1618 <ckd1618@gmail.com>
2019-06-16 17:56:46 -0700

    jsShortcuts.js

---
+++ jsShortcuts.js
@@ -0,0 +1,30 @@
+var x = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+}
+// var y = Object.entries(x);
+// var arr = ["lol", "gg"]
+// arr.push(...y) //unwraps the array y and pushes it into arr
+// console.log(arr);
+
+// for (var key in x) {
+//     console.log(key);
+// }
+
+
+// for (var i in x) {
+//     if (x.hasOwnProperty(i)) {
+//         console.log(i)
+//     }
+// }
+
+// for (var [key, val] of Object.entries(x)) {
+//     if (x.hasOwnProperty(key)) {
+//         console.log(key)
+//     }
+// }
+
+// var { one, two } = x;
+// console.log(one)
